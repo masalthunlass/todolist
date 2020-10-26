bdfdb7465a379f6895880be231a4e8e22541f38a
masalthunlass <masalthunlass@gmail.com>
2020-10-26 20:37:39 +0100

    added redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './component/App';
+import { Provider } from 'react-redux';
+
+import store from './store';
 
 const container = document.getElementById('projet');
-ReactDom.render(<App/>,  container);
+
+ReactDom.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    container
+  )
---
+++ src/reducers/index.ts
@@ -0,0 +1,6 @@
+import { Reducer } from 'redux';
+const reducer: Reducer = (state, action) => {
+    return state;
+};
+
+export default reducer;
