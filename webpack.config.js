1c73496907a559a04d065c9da1d11a89f055eafd
masalthunlass <masalthunlass@gmail.com>
2020-11-29 14:57:15 +0100

    configuration pour deboguer

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,13 @@ module.exports = {
     mode: 'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'my-first-webpack.bundle.js'
+        filename: 'my-first-webpack.bundle.js',
+        sourceMapFilename: "todolist.js.map"
     }, 
     resolve: {
         extensions: ['.js', '.ts', '.tsx']
     },
+    devtool: "source-map",
     module: {
         rules: [
             { test: /\.txt$/, use: 'raw-loader' },
